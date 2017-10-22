0531c677f5f4729d556007b8491454c8c76f82b1
wferrufino <william.ferrufino@solarwinds.com>
2017-10-22 16:47:17 -0500

    angular router updates

--- js/custom-angular.js
+++ js/custom-angular.js
@@ -1,1 +1,12 @@
-var app = angular.module("app", []); 
+var app = angular.module("app", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.html"
+    })
+    .when("/swdc", {
+        templateUrl : "swdc.html"
+    });
+    
+});
